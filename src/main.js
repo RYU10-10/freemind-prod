ee5e4f0ba15f18767a6e74d911cd1f79ffbf1fef
RYU10-10 <g020c1072@g.neec.ac.jp>
2023-12-15 14:18:05 +0900

    日時の表示の追加

--- src/main.js
+++ src/main.js
@@ -20,10 +20,13 @@ const send = document.getElementById("send");
 const naiyou = document.getElementById("naiyou");
 const list = document.getElementById("list");
 const postsRef = ref(db, "users/post");
+const now = new Date().toISOString();
+//const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
 
 function AddData() {
   const newPostRef = push(postsRef, {
     post: naiyou.value,
+    time: now,
   })
     .then(() => {
       updateView();
@@ -35,12 +38,18 @@ function AddData() {
     });
 }
 window.showPost = function (elem) {
-  const post = elem.innerHTML;
+  const key = elem.dataset.key; // 投稿のキーをデータ属性に
+  sessionStorage.setItem("selectedKey", key);
 
+  const post = elem.querySelector("h3").innerHTML;
+
+  sessionStorage.setItem("selectedPost", post);
+  window.location.href = "mypage.html";
   document.getElementById("post-viewer").innerHTML = `
     <h1>${post}</h1>
   `;
 };
+
 function updateView() {
   var postsHTML = "";
   onValue(
@@ -49,9 +58,14 @@ function updateView() {
       if (snapshot.exists()) {
         console.log(snapshot.val());
         snapshot.forEach((child) => {
+          const postData = child.val();
+          const formattedDate = formatTimestamp(postData.time);
+          console.log("タイムスタンプ:", postData.time);
+          console.log("フォーマットされた日時:", formattedDate);
           postsHTML += `
-          <div onclick="showPost(this)">
+          <div onclick="showPost(this)" data-key="${child.key}">
             <h3>${child.val().post}</h3>
+            <p>${formattedDate}</p>
             </div>
           `;
         });
@@ -61,5 +75,17 @@ function updateView() {
     (error) => console.log(error)
   );
 }
+
+function formatTimestamp(timestamp) {
+  const dateObject = new Date(timestamp);
+  const year = dateObject.getFullYear();
+  const month = ("0" + (dateObject.getMonth() + 1)).slice(-2);
+  const day = ("0" + dateObject.getDate()).slice(-2);
+  const hours = ("0" + dateObject.getHours()).slice(-2);
+  const minutes = ("0" + dateObject.getMinutes()).slice(-2);
+
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
+}
+
 updateView();
 send.addEventListener("click", AddData);
